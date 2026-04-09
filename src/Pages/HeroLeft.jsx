cfac98a09d359edf8ad50edbba07faf2983dc168
Aditya Kr Sharma <adit.sharma.1136@gmail.com>
2026-04-09 09:31:53 +0530

    today

--- src/Pages/HeroLeft.jsx
+++ src/Pages/HeroLeft.jsx
@@ -11,7 +11,7 @@ const HeroLeft = () => {
             <h1 className='text-[2rem] font-semibold'>Min. 35-75% Off</h1>
             <div className='flex gap-10 mt-[4rem]'>
                 <button onClick={()=> {navigate('/shop'); scrollTo(0,0)}} className='hover:scale-95 active:bg-white active:border-2 active:border-[#0066CD] active:text-[#0066CD] py-2 px-6 text-[0.8rem] font-semibold bg-[#0066CD] text-white'>SHOP NOW</button>
-                <button className='hover:scale-95 py-2 px-6 text-[0.8rem] font-semibold bg-white border-2 border-[#0066CD] text-[#0066CD]'>READ MORE</button>
+                <button onClick={()=> {navigate('/about'); scrollTo(0,0)}} className='hover:scale-95 py-2 px-6 text-[0.8rem] font-semibold bg-white border-2 border-[#0066CD] text-[#0066CD]'>READ MORE</button>
             </div>
         </div>
     )
